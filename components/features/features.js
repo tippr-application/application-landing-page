110734b97c71e43ad12c194ea625882552c3acdb
joepak <josephpak2014@gmail.com>
2019-02-07 12:28:05 -0500

    Deprecating Feature boad and Testimonials JS

--- components/features/features.js
+++ components/features/features.js
@@ -1,37 +1,37 @@
-class Feature {
-    constructor(element) {
-        this.element = element;
+// class Feature {
+//     constructor(element) {
+//         this.element = element;
 
-        this.data = this.element.dataset.feature;
+//         this.data = this.element.dataset.feature;
 
-        this.contentElement = document.querySelector(`.feature-text[data-feature="${this.element.dataset.feature}"]`)
+//         this.contentElement = document.querySelector(`.feature-text[data-feature="${this.element.dataset.feature}"]`)
         
-        this.featureContent = new FeatureContent(this.contentElement);
+//         this.featureContent = new FeatureContent(this.contentElement);
 
-        this.element.addEventListener("click", () => this.select())
+//         this.element.addEventListener("click", () => this.select())
 
-    }
+//     }
 
-    select() {
-        document.querySelectorAll(".feature-imgs img").forEach(element => element.classList.remove("feature-img-selected"));
+//     select() {
+//         document.querySelectorAll(".feature-imgs img").forEach(element => element.classList.remove("feature-img-selected"));
 
-        this.element.classList.add("feature-img-selected");
+//         this.element.classList.add("feature-img-selected");
 
-        this.featureContent.select();
-    }
-}
+//         this.featureContent.select();
+//     }
+// }
 
-class FeatureContent {
-    constructor(element) {
-        this.element = element;
-    }
+// class FeatureContent {
+//     constructor(element) {
+//         this.element = element;
+//     }
 
-    select() {
-        document.querySelectorAll(".feature-text").forEach(e => e.classList.remove("feature-text-selected"));
+//     select() {
+//         document.querySelectorAll(".feature-text").forEach(e => e.classList.remove("feature-text-selected"));
 
-        this.element.classList.add("feature-text-selected");
-        this.element.classList.remove("text-hidden");
-    }
-}
+//         this.element.classList.add("feature-text-selected");
+//         this.element.classList.remove("text-hidden");
+//     }
+// }
 
-const features = document.querySelectorAll(".feature-imgs img").forEach(feature => new Feature(feature))
+// const features = document.querySelectorAll(".feature-imgs img").forEach(feature => new Feature(feature))
--- components/testimonials/testimonials.js
+++ components/testimonials/testimonials.js
@@ -1,18 +1,18 @@
-class Testimonial {
-    constructor(element) {
-        this.element = element;
+// class Testimonial {
+//     constructor(element) {
+//         this.element = element;
 
-        this.element.addEventListener("mouseover", () => this.squeeze());
-        this.element.addEventListener("mouseout", () => this.unsqueeze());
-    }
+//         this.element.addEventListener("mouseover", () => this.squeeze());
+//         this.element.addEventListener("mouseout", () => this.unsqueeze());
+//     }
 
-    squeeze() {
-        this.element.style.justifyContent = "space-evenly";
-    }
+//     squeeze() {
+//         this.element.style.justifyContent = "space-evenly";
+//     }
 
-    unsqueeze() {
-        this.element.style.justifyContent = "space-around";
-    }
-}
+//     unsqueeze() {
+//         this.element.style.justifyContent = "space-around";
+//     }
+// }
 
-const testimonials = Array.from(document.querySelectorAll(".board-row")).map(element => element).forEach(element => new Testimonial(element));
+// const testimonials = Array.from(document.querySelectorAll(".board-row")).map(element => element).forEach(element => new Testimonial(element));
